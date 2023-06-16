85d6383a314e9a924a5f11ec16fd872b10c3942b
LiuChe <che2000@buaa.edu.cn>
2023-06-16 21:10:17 +0800

    add cross validation

--- Data_loader.py
+++ Data_loader.py
@@ -1,117 +1,118 @@
 import torch
 import numpy as np
 from scipy.io import loadmat
+from sklearn.model_selection import KFold
 from tensorflow.keras import utils as np_utils
+
+
 class BaseData_loader:
-    def __init__(self,n_input,n_data):
+    def __init__(self, n_input, n_data):
         pass
-        self.train_data=self.generate_train_data(n_input,n_data)
-        self.test_data=self.generate_test_data(n_input,n_data)
-
-
-    def generate_train_data(self,num_input,n_data):
-        train_data=torch.rand(n_data,num_input)
-        return train_data
-    def generate_test_data(self,num_input,n_data):
-        test_data = torch.rand(n_data, num_input)
-        return test_data
-
-class Data_loader(BaseData_loader):
-    def __init__(self,n_input=0,n_data=0,windows_width=500,windows_step=20,num_trials=10):
-        #super().__init__(n_input=n_input,n_data=n_data)
-        self.windows_width=windows_width
-        self.windows_step=windows_step
-        self.num_trials=num_trials
         self.train_data = self.generate_train_data(n_input, n_data)
         self.test_data = self.generate_test_data(n_input, n_data)
 
+    def generate_train_data(self, num_input, n_data, train_index=None):
+        train_data = torch.rand(n_data, num_input)
+        return train_data
 
-    def generate_train_data(self,num_input,n_data):
+    def generate_test_data(self, num_input, n_data, test_index=None):
+        test_data = torch.rand(n_data, num_input)
+        return test_data
+
+
+class Data_loader(BaseData_loader):
+    def __init__(self, EEG_path, n_input=0, n_data=0, windows_width=500, windows_step=20, num_trials=10):
+        # super().__init__(n_input=n_input,n_data=n_data)
+        self.windows_width = windows_width
+        self.windows_step = windows_step
+        self.num_trials = num_trials
         ##################### Process, filter and epoch the data ######################
-        EEG_path = "./data/SSVEP/S1.mat"  # "/nfs/diskstation/DataStation/CheLiu/data/SSVEP/S1.mat"
-
         # ‘Electrode index’, ‘Time points’, ‘Target index’, and ‘Block index’
         EEG_data = loadmat(EEG_path)['data']  # [64, 1500, 40, 6]
         EEG_data = EEG_data.transpose(3, 2, 0, 1)  # [6, 40, 64, 1500]
 
-        self.EEG_data=EEG_data
-        num_blocks, num_trials, chans, samples = EEG_data.shape
-        num_trials=self.num_trials
-        kernels = 1
-        X = EEG_data[:,:num_trials,:,:]  # [6, 40, 64, 1500]
-        y = np.array(list(range(num_trials)) * num_blocks).reshape(num_blocks, -1)  # [6, 40]
-
-        # train/validate/test
-        X_train_data = X[:5, ...].reshape(-1, chans, samples) # [200, 64, 1500]
-        Y_train_data = y[:5, ...].reshape(-1) #[200]
-        X_train=np.zeros((X_train_data.shape[0],int((samples-self.windows_width)/self.windows_step),chans,self.windows_width ))
-        Y_train=np.zeros((Y_train_data.shape[0],int((samples-self.windows_width)/self.windows_step) ))
-        for i in range(X_train_data.shape[0]):
-            for index,start_index in enumerate([x*self.windows_step for x in range(int((samples-self.windows_width)/self.windows_step))]):
-                X_train[i,index,:,:]=X_train_data[i,:,start_index:start_index+self.windows_width]
-                Y_train[i,index]=Y_train_data[i]
-        X_train = X_train.reshape(-1, chans, self.windows_width)  # [200*100, 64, 500]
-        Y_train = Y_train.reshape(-1) # [200*100]
-        ############################# EEGNet portion ##################################
-
-        # convert labels to one-hot encodings.
-        Y_train = np_utils.to_categorical(Y_train) #[200*100,40]
-
-        # convert data to NHWC (trials, channels, samples, kernels) format. Data
-        # contains 60 channels and 151 time-points. Set the number of kernels to 1.
-        X_train = X_train.reshape(X_train.shape[0], chans, self.windows_width, kernels) #[200*100, 64, 500,1]
-
-
-        print('X_train shape:', X_train.shape) # [200*100, 64, 500,1] 200表示5个block乘40个类别
-        print(X_train.shape[0], 'train samples')
-
-        return (X_train,Y_train)
-    def generate_test_data(self,num_input,n_data):
-        ##################### Process, filter and epoch the data ######################
-
-        EEG_path = "./data/SSVEP/S1.mat"  # "/nfs/diskstation/DataStation/CheLiu/data/SSVEP/S1.mat"
-
-        # ‘Electrode index’, ‘Time points’, ‘Target index’, and ‘Block index’
-        EEG_data = loadmat(EEG_path)['data']  # [64, 1500, 40, 6]
-        EEG_data = EEG_data.transpose(3, 2, 0, 1)  # [6, 40, 64, 1500]
-
-        num_blocks, num_trials, chans, samples = EEG_data.shape
+        num_blocks, num_trials, self.chans, self.samples = EEG_data.shape
         num_trials = self.num_trials
-        kernels = 1
+        self.kernels = 1
         # # extract raw data. scale by 1000 due to scaling sensitivity in deep learning
         # X = EEG_data * 1000  # format is in (trials, channels, samples)
         # y = EEG_data
-        X = EEG_data[:,:num_trials,:,:]  # [6, 40, 64, 1500]
-        y = np.array(list(range(num_trials)) * num_blocks).reshape(num_blocks, -1)  # [6, 40]
+        channel_list = [60, 61, 62, 47, 53, 54, 55, 56, 57]
+        self.X = EEG_data[:, :num_trials, channel_list, :]  # [6, 10, 9, 1500]
+        self.chans = 9
+        self.y = np.array(list(range(num_trials)) * num_blocks).reshape(num_blocks, -1)  # [6, 40]
 
+    def get_data(self):
+        kf = KFold(n_splits=6, shuffle=False)  # 初始化KFold
+        data = []
+        for train_index, test_index in kf.split(self.X):  # 调用split方法切分数据
+            # print('train_index:%s , test_index: %s ' % (train_index, test_index))
+            X_train, Y_train = self.generate_train_data(num_input=0, n_data=0, train_index=train_index)
+            X_test, Y_test = self.generate_test_data(num_input=0, n_data=0, test_index=test_index)
+            data.append((X_train, Y_train, X_test, Y_test))
+        return data
 
-        X_test_data = X[5, ...] # [40,64,1500]
-        Y_test_data = y[5, :] # [40]
-        X_test = np.zeros((X_test_data.shape[0], int((samples - self.windows_width) / self.windows_step), chans, self.windows_width))
-        Y_test = np.zeros((Y_test_data.shape[0], int((samples - self.windows_width) / self.windows_step)))
-        for i in range(X_test_data.shape[0]):
+    def generate_train_data(self, num_input, n_data, train_index=None):
+        samples = self.samples
+
+        # train/validate/test
+        X_train_data = self.X[train_index].reshape(-1, self.chans, samples)  # [200, 64, 1500]
+        Y_train_data = self.y[train_index].reshape(-1)  # [200]
+        X_train = np.zeros(
+            (X_train_data.shape[0], int((samples - self.windows_width) / self.windows_step), self.chans, self.windows_width))
+        Y_train = np.zeros((Y_train_data.shape[0], int((samples - self.windows_width) / self.windows_step)))
+        for i in range(X_train_data.shape[0]):
             for index, start_index in enumerate(
                     [x * self.windows_step for x in range(int((samples - self.windows_width) / self.windows_step))]):
-                X_test[i, index, :, :] = X_test_data[i, :, start_index:start_index + self.windows_width]
-                Y_test[i, index] = Y_test_data[i]
-        X_test = X_test.reshape(-1, chans, self.windows_width)  # [40*100, 64, 500]
-        Y_test = Y_test.reshape(-1)  # [40*100]
+                X_train[i, index, :, :] = X_train_data[i, :, start_index:start_index + self.windows_width]
+                Y_train[i, index] = Y_train_data[i]
+        X_train = X_train.reshape(-1, self.chans, self.windows_width)  # [200*100, 64, 500]
+        noise = np.random.normal(0, 1.0, size=X_train.shape).astype(np.float32)
+        percentage = 0.0
+        mask = np.random.choice([0, 1], size=X_train.shape, p=[1 - percentage, percentage])
+        X_train += noise * mask
+        Y_train = Y_train.reshape(-1)  # [200*100]
+        ############################# EEGNet portion ##################################
 
+        # convert labels to one-hot encodings.
+        Y_train = np_utils.to_categorical(Y_train)  # [200*100,40]
+
+        # convert data to NHWC (trials, channels, samples, kernels) format. Data
+        # contains 60 channels and 151 time-points. Set the number of kernels to 1.
+        X_train = X_train.reshape(X_train.shape[0], self.chans, self.windows_width, self.kernels)  # [200*100, 64, 500,1]
+
+        # print('X_train shape:', X_train.shape)  # [200*100, 64, 500,1] 200表示5个block乘40个类别
+        # print(X_train.shape[0], 'train samples')
+
+        return X_train, Y_train
+
+    def generate_test_data(self, num_input, n_data, test_index=None):
+        X_test_data = self.X[test_index].reshape(-1, self.chans, self.samples)  # [40,64,500]
+        Y_test_data = self.y[test_index].reshape(-1)  # [40]
+        # X_test = np.zeros((X_test_data.shape[0], int((samples - self.windows_width) / self.windows_step), chans, self.windows_width))
+        # Y_test = np.zeros((Y_test_data.shape[0], int((samples - self.windows_width) / self.windows_step)))
+        # for i in range(X_test_data.shape[0]):
+        #     for index, start_index in enumerate(
+        #             [x * self.windows_step for x in range(int((samples - self.windows_width) / self.windows_step))]):
+        #         X_test[i, index, :, :] = X_test_data[i, :, start_index:start_index + self.windows_width]
+        #         Y_test[i, index] = Y_test_data[i]
+        # X_test = X_test.reshape(-1, chans, self.windows_width)  # [40*100, 64, 500]
+        # Y_test = Y_test.reshape(-1)  # [40*100]
+        X_test = X_test_data[:, :, self.samples - self.windows_width:self.samples]
+        Y_test = Y_test_data
         ############################# EEGNet portion ##################################
 
         # Y_validate = np_utils.to_categorical(Y_validate)
         Y_test = np_utils.to_categorical(Y_test)
 
-
         # X_validate = X_validate.reshape(X_validate.shape[0], chans, samples, kernels)
-        X_test = X_test.reshape(X_test.shape[0], chans, self.windows_width, kernels)
+        X_test = X_test.reshape(X_test.shape[0], self.chans, self.windows_width, self.kernels)
 
+        # print(X_test.shape[0], 'test samples')
 
-        print(X_test.shape[0], 'test samples')
+        return X_test, Y_test
 
-        return (X_test, Y_test)
-
-    def data_generator(self,n_data):  # z是自变量 ，y是因变量。y: dim*samples
+    def data_generator(self, n_data):  # z是自变量 ，y是因变量。y: dim*samples
         ##################### Process, filter and epoch the data ######################
         EEG_path = "./data/SSVEP/S1.mat"  # "/nfs/diskstation/DataStation/CheLiu/data/SSVEP/S1.mat"
 
@@ -153,7 +154,8 @@ class Data_loader(BaseData_loader):
         print(X_test.shape[0], 'test samples')
         return (z, y)
 
-if __name__=='__main__':
-    data_loader=Data_loader(num_input=1,
-                            n_data=50)
+
+if __name__ == '__main__':
+    data_loader = Data_loader(num_input=1,
+                              n_data=50)
     data_loader.generate_train_data
--- main.py
+++ main.py
@@ -27,71 +27,99 @@ from matplotlib import pyplot as plt
 # to be explicit in case if the user has changed the default ordering
 K.set_image_data_format('channels_last')
 
-data_loader=Data_loader(windows_width=500,windows_step=20,num_trials=10) # 使用windows_width=1500-1,windows_step=1时，相当于没有滑动窗口处理。num_trials表示取40类中的几个类别。
+total_info = []
+for sub in range(1, 6):
+    EEG_path = "/nfs/diskstation/DataStation/CheLiu/data/SSVEP/S%d.mat" % sub
+    data_loader=Data_loader(windows_width=300,windows_step=50,num_trials=10, EEG_path=EEG_path) # 使用windows_width=1500-1,windows_step=1时，相当于没有滑动窗口处理。num_trials表示取40类中的几个类别。
 
-num_blocks, num_trials, chans, samples = data_loader.EEG_data.shape
+    # num_blocks, num_trials, chans, samples = data_loader.EEG_data.shape
+    acc_list = []
+    for cv, (X_train, Y_train, X_test, Y_test) in enumerate(data_loader.get_data()):
+        print('------------------------------------')
+        print('CV %d begins:' % (cv + 1))
+        model = EEGNet_SSVEP(nb_classes=data_loader.num_trials, Chans=data_loader.chans, Samples=data_loader.windows_width,
+                             dropoutRate=0.5, kernLength=256, F1=96,
+                             D=1, F2=96, dropoutType='Dropout')
 
-X_train,Y_train=data_loader.train_data
-X_test,Y_test=data_loader.test_data
+        # compile the model and set the optimizers
+        model.compile(loss='categorical_crossentropy', optimizer='adam',
+                      metrics=['accuracy'])
 
-model = EEGNet_SSVEP(nb_classes=data_loader.num_trials, Chans=chans, Samples=data_loader.windows_width,
-                     dropoutRate=0.5, kernLength=256, F1=96,
-                     D=1, F2=96, dropoutType='Dropout')
+        # count number of parameters in the model
+        numParams = model.count_params()
 
-# compile the model and set the optimizers
-model.compile(loss='categorical_crossentropy', optimizer='adam',
-              metrics=['accuracy'])
+        # set a valid path for your system to record model checkpoints
+        checkpointer = ModelCheckpoint(filepath='./tmp/checkpoint.h5', verbose=1,
+                                       save_best_only=False)
 
-# count number of parameters in the model
-numParams = model.count_params()
+        ###############################################################################
+        # if the classification task was imbalanced (significantly more trials in one
+        # class versus the others) you can assign a weight to each class during
+        # optimization to balance it out. This data is approximately balanced so we
+        # don't need to do this, but is shown here for illustration/completeness.
+        ###############################################################################
 
-# set a valid path for your system to record model checkpoints
-checkpointer = ModelCheckpoint(filepath='./tmp/checkpoint.h5', verbose=1,
-                               save_best_only=False)
+        # the syntax is {class_1:weight_1, class_2:weight_2,...}. Here just setting
+        # the weights all to be 1
+        class_weights = {0: 1, 1: 1, 2: 1, 3: 1}
+        result_list = []
+        for i in range(100): # train 10 epochs
+            ################################################################################
+            # fit the model. Due to very small sample sizes this can get
+            # pretty noisy run-to-run, but most runs should be comparable to xDAWN +
+            # Riemannian geometry classification (below)
+            ################################################################################
+            # fittedModel = model.fit(X_train, Y_train, batch_size=16, epochs=300,
+            #                         verbose=2, validation_data=(X_validate, Y_validate),
+            #                         callbacks=[checkpointer], class_weight=class_weights)
+            fittedModel = model.fit(X_train, Y_train, batch_size=16, epochs=1,
+                                    verbose=2, callbacks=[checkpointer])
 
-###############################################################################
-# if the classification task was imbalanced (significantly more trials in one
-# class versus the others) you can assign a weight to each class during
-# optimization to balance it out. This data is approximately balanced so we
-# don't need to do this, but is shown here for illustration/completeness.
-###############################################################################
+            # load optimal weights
+            #model.load_weights('./tmp/checkpoint.h5')
 
-# the syntax is {class_1:weight_1, class_2:weight_2,...}. Here just setting
-# the weights all to be 1
-class_weights = {0: 1, 1: 1, 2: 1, 3: 1}
+            ###############################################################################
+            # can alternatively used the weights provided in the repo. If so it should get
+            # you 93% accuracy. Change the WEIGHTS_PATH variable to wherever it is on your
+            # system.
+            ###############################################################################
 
-for i in range(100): # train 10 epochs
-    ################################################################################
-    # fit the model. Due to very small sample sizes this can get
-    # pretty noisy run-to-run, but most runs should be comparable to xDAWN +
-    # Riemannian geometry classification (below)
-    ################################################################################
-    # fittedModel = model.fit(X_train, Y_train, batch_size=16, epochs=300,
-    #                         verbose=2, validation_data=(X_validate, Y_validate),
-    #                         callbacks=[checkpointer], class_weight=class_weights)
-    fittedModel = model.fit(X_train, Y_train, batch_size=16, epochs=1,
-                            verbose=2, callbacks=[checkpointer])
+            # WEIGHTS_PATH = /path/to/EEGNet-8-2-weights.h5
+            # model.load_weights(WEIGHTS_PATH)
 
-    # load optimal weights
-    #model.load_weights('./tmp/checkpoint.h5')
+            ###############################################################################
+            # make prediction on test set.
+            ###############################################################################
 
-    ###############################################################################
-    # can alternatively used the weights provided in the repo. If so it should get
-    # you 93% accuracy. Change the WEIGHTS_PATH variable to wherever it is on your
-    # system.
-    ###############################################################################
+            probs = model(X_test).numpy()
+            preds = probs.argmax(axis=-1)
+            print('pred: ', preds)
+            acc = np.mean(preds == Y_test.argmax(axis=-1))
+            result_list.append(acc)
+            print("Epoch %d: Classification accuracy: %f " % (i+1, acc))
+        print('\n----------------')
+        best_result = np.max(result_list)
+        acc_list.append(best_result)
+        print('Best result: %f' % best_result)
+        print('------------------------------------\n\n\n')
 
-    # WEIGHTS_PATH = /path/to/EEGNet-8-2-weights.h5
-    # model.load_weights(WEIGHTS_PATH)
+    print('\n------------------- s%d -------------------------------------------------' % sub)
+    acc_min = np.min(acc_list)
+    acc_max = np.max(acc_list)
+    acc_mean = np.mean(acc_list)
+    acc_list.append(acc_min)
+    acc_list.append(acc_max)
+    acc_list.append(acc_mean)
+    row_str = 'S%d & ' % sub + ' & '.join([str(np.round(accuracy * 100, 2)) for accuracy in acc_list])
+    total_info.append(row_str)
+    print(row_str)
+    print('------------------------------------------------------------------------')
+    print('------------------------------------------------------------------------')
+    print('------------------------------------------------------------------------\n')
 
-    ###############################################################################
-    # make prediction on test set.
-    ###############################################################################
-
-    probs = model.predict(X_test)
-    preds = probs.argmax(axis=-1)
-    acc = np.mean(preds == Y_test.argmax(axis=-1))
-    print("Classification accuracy: %f " % (acc))
+print('\n\n\n\n\n-----------------------------TOTAL--------------------------------------')
+print('------------------------------------------------------------------------')
+print('\n'.join(total_info))
 
 '''
 ############################# PyRiemann Portion ##############################
